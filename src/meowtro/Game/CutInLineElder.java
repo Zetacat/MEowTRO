c168aa097ad1719a5e036a0f966642fb23cff690
Zetacat <k94155@gmail.com>
2021-06-26 15:48:19 +0800

    rename Meowtro to meowtro

---
+++ src/meowtro/Game/CutInLineElder.java
@@ -0,0 +1,25 @@
+package meowtro.game;
+
+import meowtro.Position;
+import meowtro.metro_system.*;
+
+public class CutInLineElder extends Passenger {
+    
+    public CutInLineElder(Region birthRegion, long spawnTime, Position position, Station destinationStation) {
+        super(birthRegion, spawnTime, position, destinationStation);
+    }
+
+    @Override
+    public void enterStation(Station station) {
+        this.position = station.getPosition();
+        // arrive station
+        if (station == this.destinationStation) {
+            this.arriveDestination();
+        }
+        // enter station and wait
+        else {
+            station.insertPassenger(this, 0);
+            this.currentStation = station;
+        }
+    }
+}
