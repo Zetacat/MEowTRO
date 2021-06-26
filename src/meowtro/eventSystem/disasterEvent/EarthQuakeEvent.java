3d41ed2b81ecac51d5a4128a55186714e7ecaa9c
DarrenLiou <b06902008@ntu.edu.tw>
2021-06-27 01:18:54 +0800

    add Earthquake event

--- src/meowtro/eventSystem/disasterEvent/EarthQuakeEvent.java
+++ src/meowtro/eventSystem/disasterEvent/EarthQuakeEvent.java
@@ -1,11 +1,21 @@
 package meowtro.eventSystem.disasterEvent;
 import meowtro.game.City;
+import java.util.List;
+import meowtro.metro_system.railway.*;
 
 public class EarthQuakeEvent extends DisasterEvent{
-    public EarthQuakeEvent(City city, String name, String happenedTimeString, int numOfDestroyRailway){
+    private int remainTimeUnit;
+    public EarthQuakeEvent(City city, String name, String happenedTimeString, int numOfDestroyRailway, int remainTimeUnit){
         super(city, name, happenedTimeString, numOfDestroyRailway);
+        this.remainTimeUnit = remainTimeUnit;
     }
     public void trigger(){
-        
+        List<Line> allLines = this.city.getAllLines();
+        for(Line l : allLines){
+            List<Railway> allRailways = l.getRailways();
+            for(Railway r: allRailways){
+                r.setRemainTimeToLive(this.remainTimeUnit);
+            }
+        }
     }
 }
