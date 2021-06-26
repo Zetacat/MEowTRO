ba8216deb200cb107adc813ef7d8304a4b1288cb
shaoyu0966 <b06705028@ntu.edu.tw>
2021-06-26 19:19:59 +0800

    combine default and local config

--- src/meowtro/game/Config.java
+++ src/meowtro/game/Config.java
@@ -3,22 +3,39 @@ package meowtro.game;
 import java.util.Properties;
 import java.util.Set;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Config {
 
-    String config_path = null;
+    String defaultConfigPath = null;
     private Properties properties = new Properties();
 
-    public Config(String config_path) {
+    public Config(String defaultConfigPath, String localConfigPath) {
         try {
-            // load .properties
-            FileInputStream in = new FileInputStream(config_path);
+            // load default config
+            FileInputStream in = new FileInputStream(defaultConfigPath);
             this.properties.load(in);
             in.close();
+
+            if (localConfigPath != null) {
+                // load local config
+                in = new FileInputStream(localConfigPath);
+                Properties localProperties = new Properties();
+                localProperties.load(in);
+                in.close();
+    
+                // merge local config to this.properties
+                for (Object key: localProperties.keySet()) {
+                    String keyStr = (String) key;
+                    if (this.properties.containsKey(keyStr)) {
+                        this.properties.setProperty(keyStr, localProperties.getProperty(keyStr));
+                    }
+                }
+            }
+
             // store config_path for writing back
-            this.config_path = config_path;
+            this.defaultConfigPath = defaultConfigPath;
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -27,27 +44,18 @@ public class Config {
     public String get(String key) {
         return this.properties.getProperty(key);
     }
-    
-    public void set(String key, String value) {
-        this.properties.setProperty(key, value);
-        try {
-            FileOutputStream out = new FileOutputStream(this.config_path);
-            this.properties.store(out, null);
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public Set<String> getAllKeys() {
         return this.properties.stringPropertyNames();
     }
+
+    public void printConfig() {
+        this.properties.list(System.out);
+    }
     
     /****** MAIN ******/
     public static void main(String[] args) {
-        Config config = new Config("./config.properties");
-        System.out.println(config.get("mountain.r") + ", " + config.get("mountain.g") + ", " + config.get("mountain.b"));
-        config.set("mountain.r", "19");
-        System.out.println(config.get("mountain.r") + ", " + config.get("mountain.g") + ", " + config.get("mountain.b"));
+        Config config = new Config("./config.properties", "./local.properties");
+        config.printConfig();
     }
 }
