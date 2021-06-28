1f65e897b044caa4c157d3bdf04790de205e8407
DarrenLiou <b06902008@ntu.edu.tw>
2021-06-28 21:11:19 +0800

    add maxTimeto live

--- src/meowtro/metro_system/railway/RailwayManager.java
+++ src/meowtro/metro_system/railway/RailwayManager.java
@@ -3,12 +3,16 @@ package meowtro.metro_system.railway;
 import java.util.ArrayList;
 import java.util.List;
 
+import meowtro.game.Game;
 import meowtro.game.entityManager.*;
 import meowtro.metro_system.station.Station;
 
 public class RailwayManager extends EntityManager{
+    private long maxTimeToLive;
+    public RailwayManager(){
+        this.maxTimeToLive = Long.parseLong(Game.getConfig().get("metro_system.railway.maxTimeToLive"));
+    }
     List<Railway> railways = new ArrayList<Railway>(); 
-
     public void build(Station s1, Station s2, Line line){
         if (s1.isAdjTo(s2) || s2.isAdjTo(s1)){
             return; 
@@ -18,6 +22,6 @@ public class RailwayManager extends EntityManager{
                 return; 
             }
         }
-        new Railway(s1, s2, line);
+        new Railway(s1, s2, line, this.maxTimeToLive);
     }
 }
